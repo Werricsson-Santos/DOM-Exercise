93ad3a87647d8cea0e0edcfe45c9e69306f537d5
Werricsson <werricsson.santos@hotmail.com>
2022-10-29 17:43:36 -0300

    CTD - Função para capturar o cadastro (ainda precisa de correção para capturar as techSkills)

--- src/index.js
+++ src/index.js
@@ -2,89 +2,115 @@
 const addSkill = document.getElementById('add')
 const removeSkill = document.getElementById('remove')
 const submit = document.getElementById('submit')
+const form = document.getElementById('form-content')
 const developers = []
 let inputRows = 0
 
 
 // Eventos e Funções
 addSkill.addEventListener('click', function () {
-    const rowIndex = inputRows
-    inputRows++
+        const rowIndex = inputRows
+        inputRows++
 
-    const tecnologies = document.getElementById('techSkills')
+        const tecnologies = document.getElementById('techSkills')
 
-    const inputTechSkills = document.createElement('input')
-    inputTechSkills.className = 'form-control mt-4'
-    inputTechSkills.id = 'inputRow-' + rowIndex
-    inputTechSkills.value = ''
-    inputTechSkills.type = 'text'
-    inputTechSkills.name = 'technology'
-    inputTechSkills.placeholder = 'Insira uma tecnologia que você utiliza:'
-    inputTechSkills.ariaLabel = 'input-tech-skill'
+        const inputTechSkills = document.createElement('input')
+                inputTechSkills.className = 'form-control mt-4'
+                inputTechSkills.id = 'inputRow-' + rowIndex
+                inputTechSkills.value = ''
+                inputTechSkills.type = 'text'
+                inputTechSkills.name = 'techName'
+                inputTechSkills.placeholder = 'Insira uma tecnologia que você utiliza:'
+                inputTechSkills.ariaLabel = 'input-tech-skill'
 
 
-    const workedTimeUl = document.createElement('ul')
-    workedTimeUl.className = 'form-check d-flex mt-2'
-    
+        const workedTimeUl = document.createElement('ul')
+                workedTimeUl.className = 'form-check d-flex mt-2'
+
 
 
         const upTwoYearsLi = document.createElement('li')
                 upTwoYearsLi.className = 'form-check'
-                upTwoYearsLi.id = 'checkboxRow' + rowIndex + '.1'
-        const upTwoYearsCheckbox = document.createElement('input')
-                upTwoYearsCheckbox.type = 'radio'
-                upTwoYearsCheckbox.className = 'form-check-input'
-                upTwoYearsCheckbox.name = 'expTimeTech'
-                upTwoYearsCheckbox.value = '0-2 anos'
-                upTwoYearsCheckbox.id = 'expCheckbox-' + rowIndex + '.1'
+                upTwoYearsLi.id = 'radioRow' + rowIndex + '.1'
+        const upTwoYearsRadio = document.createElement('input')
+                upTwoYearsRadio.type = 'radio'
+                upTwoYearsRadio.className = 'form-check-input'
+                upTwoYearsRadio.name = 'expTimeTech' + rowIndex
+                upTwoYearsRadio.value = '0-2 anos'
+                upTwoYearsRadio.id = 'expRadio-' + rowIndex + '.1'
         const upTwoYearsLabel = document.createElement('label')
                 upTwoYearsLabel.className = 'text-light'
-                upTwoYearsLabel.htmlFor = 'expCheckbox-' + rowIndex + '.1'
+                upTwoYearsLabel.htmlFor = 'expRadio-' + rowIndex + '.1'
                 upTwoYearsLabel.innerText = '0-2 anos'
-        upTwoYearsLi.append(upTwoYearsCheckbox, upTwoYearsLabel)
+        upTwoYearsLi.append(upTwoYearsRadio, upTwoYearsLabel)
         workedTimeUl.appendChild(upTwoYearsLi)
 
         const untilFourYearsLi = document.createElement('li')
                 untilFourYearsLi.className = 'form-check ms-3'
-                untilFourYearsLi.id = 'checkboxRow-' + rowIndex + '.2'
-        const untilFourYearsCheckbox = document.createElement('input')
-                untilFourYearsCheckbox.type = 'radio'
-                untilFourYearsCheckbox.className = 'form-check-input'
-                untilFourYearsCheckbox.name = 'expTimeTech'
-                untilFourYearsCheckbox.value = '3-4 anos'
-                untilFourYearsCheckbox.id = 'expCheckbox-' + rowIndex + '.2'
+                untilFourYearsLi.id = 'radioRow-' + rowIndex + '.2'
+        const untilFourYearsRadio = document.createElement('input')
+                untilFourYearsRadio.type = 'radio'
+                untilFourYearsRadio.className = 'form-check-input'
+                untilFourYearsRadio.name = 'expTimeTech' + rowIndex
+                untilFourYearsRadio.value = '3-4 anos'
+                untilFourYearsRadio.id = 'expRadio-' + rowIndex + '.2'
         const untilFourYearsLabel = document.createElement('label')
                 untilFourYearsLabel.className = 'text-light'
-                untilFourYearsLabel.htmlFor = 'expCheckbox-' + rowIndex + '.2'
+                untilFourYearsLabel.htmlFor = 'expRadio-' + rowIndex + '.2'
                 untilFourYearsLabel.innerText = '3-4 anos'
-        untilFourYearsLi.append(untilFourYearsCheckbox, untilFourYearsLabel)
+        untilFourYearsLi.append(untilFourYearsRadio, untilFourYearsLabel)
         workedTimeUl.appendChild(untilFourYearsLi)
 
         const fiveOrMoreYearsLi = document.createElement('li')
                 fiveOrMoreYearsLi.className = 'form-check ms-3 me-4'
-                fiveOrMoreYearsLi.id = 'checkboxRow' + rowIndex + '.3'
-        const fiveOrMoreYearsCheckbox = document.createElement('input')
-                fiveOrMoreYearsCheckbox.type = 'radio'
-                fiveOrMoreYearsCheckbox.className = 'form-check-input'
-                fiveOrMoreYearsCheckbox.name = 'expTimeTech'
-                fiveOrMoreYearsCheckbox.value = '5 + anos'
-                fiveOrMoreYearsCheckbox.id = 'expCheckbox-' + rowIndex + '.3'
+                fiveOrMoreYearsLi.id = 'radioRow' + rowIndex + '.3'
+        const fiveOrMoreYearsRadio = document.createElement('input')
+                fiveOrMoreYearsRadio.type = 'radio'
+                fiveOrMoreYearsRadio.className = 'form-check-input'
+                fiveOrMoreYearsRadio.name = 'expTimeTech' + rowIndex
+                fiveOrMoreYearsRadio.value = '5 + anos'
+                fiveOrMoreYearsRadio.id = 'expRadio-' + rowIndex + '.3'
         const fiveOrMoreYearsLabel = document.createElement('label')
                 fiveOrMoreYearsLabel.className = 'text-light'
-                fiveOrMoreYearsLabel.htmlFor = 'expCheckbox-' + rowIndex + '.3'
+                fiveOrMoreYearsLabel.htmlFor = 'expRadio-' + rowIndex + '.3'
                 fiveOrMoreYearsLabel.innerText = '5 + anos'
-        fiveOrMoreYearsLi.append(fiveOrMoreYearsCheckbox, fiveOrMoreYearsLabel)
+        fiveOrMoreYearsLi.append(fiveOrMoreYearsRadio, fiveOrMoreYearsLabel)
         workedTimeUl.appendChild(fiveOrMoreYearsLi)
 
         const removeBtn = document.createElement('button')
-            removeBtn.type = 'button'
-            removeBtn.className = 'btn btn-outline-danger mt-2 text-center'
-            removeBtn.innerText = 'Remover'
-            removeBtn.addEventListener('click', function (){
-                tecnologies.removeChild(inputTechSkills)
-                tecnologies.removeChild(workedTimeUl)
-                tecnologies.removeChild(removeBtn)
-            })
-    
+                removeBtn.type = 'button'
+                removeBtn.className = 'btn btn-outline-danger mt-2 text-center'
+                removeBtn.innerText = 'Remover'
+                removeBtn.addEventListener('click', function (){
+                        tecnologies.removeChild(inputTechSkills)
+                        tecnologies.removeChild(workedTimeUl)
+                        tecnologies.removeChild(removeBtn)
+        })
+
         tecnologies.append(inputTechSkills, workedTimeUl, removeBtn)
+})
+
+form.addEventListener('submit', function (ev) {
+        ev.preventDefault()
+
+        const fullnameInput = document.getElementById('fullname')
+        const inputRows = document.querySelectorAll('.form')
+
+        let technologies = []
+        inputRows.forEach(function (row) {
+                const techName = document.querySelector('#' + row.id + ' input[name="techName"]').value
+                const techExp = document.querySelector('#' + row.id + ' input[type="radio"]: checked')
+                technologies.push({ name: techName, exp: techExp})
+        })
+
+        const newDev = { fullname: fullnameInput.value, technologies: technologies }
+        developers.push(newDev)
+        alert('Dev cadastrado com sucesso!')
+
+        fullnameInput.value = ''
+        inputRows.forEach(function (row) {
+                row.remove()
+        })
+
+        console.log(developers)
 })
